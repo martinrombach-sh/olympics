0e5f47ddef000b80e21a7e384c73075822ee98b9
Martin Rombach <martinrombach88@gmail.com>
2024-07-23 18:23:12 +0100

    init service and tests

---
+++ olympics-service-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace olympics_service_test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
